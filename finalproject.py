4d66a56c737e67b4b4d4d957e7ee44c9b33f406e
smdavidson713 <43275656+smdavidson713@users.noreply.github.com>
2019-01-01 21:35:16 -0500

    Add files via upload

---
+++ finalproject.py
@@ -0,0 +1,216 @@
+def blankLine():
+	print(" ")
+def main():
+	story = input("Which story do you want to do? \n" +
+				  		"ALIEN ABDUCTION" + "\n" +
+				  		"THE ART OF ESPIONAGE" + "\n" +
+				  		"WHO IS THE REAL EASTER BUNNY?" + "\n" +
+				  	"Please type in the title: " )
+
+
+	def madLib_1():
+		words = []
+
+		words.append(input("Profession: "))
+		words.append(input("Verb ending in -ing: "))
+		words.append(input("Adjective: "))
+		words.append(input("Noun: "))
+		words.append(input("Verb ending in -ing: "))
+		words.append(input("Silly word: "))
+		words.append(input("Body part: "))
+		words.append(input("Place: "))
+		words.append(input("Color: "))
+		words.append(input("Medical procedure: "))
+		words.append(input("Celebrity name: "))
+		words.append(input("Object: "))
+		words.append(input("Number: "))
+		words.append(input("Substance: "))
+		words.append(input("Emotion: "))
+		words.append(input("Adjective: "))
+		words.append(input("Number: "))
+		words.append(input("Plural noun: "))
+		words.append(input("Personality trait: "))
+
+		words = [x.upper() for x in words]
+
+		blankLine()
+
+		print("All Done!")
+
+		blankLine()
+
+
+		print("Alien Abudction:")
+		blankLine()
+		print("As a " + words[0] + " with rich experience in " + words[1] + ", " + "\n" +
+			  "I never considered myself a prime candidate to become an FBI agent." + "\n" +
+			  "However it all changed on a " + words[2] + " summer night when I encountered" + "\n" +
+			  "a flying " + words[3] + " while " + words [4] + " in my backyard." + "\n" +
+			  "Before I could yell " + words[5] + ", my " + words[6] + " got caught in a" + "\n" +
+			  "bright beam of light, and I was teleported to " + words[7] + ". There, I saw" + "\n" +
+			  "little " + words[8] + " beings preforming a " + words[9] + " on a person who" + "\n" +
+			  "looked like " + words[10] + ". No, it was " + words[10] + "!" + "\n" +
+			  "Without hesitation, I grabbed a " + words[11] + " and attacked them." + "\n" +
+			  "I must have wounded at least " + words[12] + " of the aliends before they " + "\n" +
+			  "retreated, leaving only pools of " + words[13] + " behind. I could hear " + "\n" +
+			  "sirens in the distance. " + words[10] + " was looking at me in absolute " + "\n" +
+			  words[14] + ". 'Are you okay?' I asked. " + words[10] + " answered: 'I'm " + "\n" +
+			  words[15] + ", thanks!' At that point, " + words[16] + " government agents burst" + "\n" +
+			  "in, armed with " + words[17] + ", impressed by my " + words[18] + "," + "\n" +
+			  "they recruited me into the FBI on the spot.")
+		blankLine()
+		print("The end.")
+
+	def madLib_2():
+		words = []
+
+		words.append(input("Verb ending in -ing: "))
+		words.append(input("Adjective: "))
+		words.append(input("Adjective: "))
+		words.append(input("Plural noun: "))
+		words.append(input("Adjective: "))
+		words.append(input("Person in room: "))
+		words.append(input("Plural noun: "))
+		words.append(input("Place: "))
+		words.append(input("Adjective: "))
+		words.append(input("Celebrity: "))
+		words.append(input("Noun: "))
+		words.append(input("Plural noun: "))
+		words.append(input("Adjective: "))
+		words.append(input("Plural noun: "))
+		words.append(input("Plural noun: "))
+		words.append(input("Noun: "))
+		words.append(input("Plural noun: "))
+
+		words = [x.upper() for x in words]
+
+		blankLine()
+
+		print("All Done!")
+
+		blankLine()
+
+
+		print("The Art of Espionage:")
+		blankLine()
+		print("Espionage is the formal word for " + words[0] + ". In the " + "\n" +
+		      "shadowy world of spies, a/an " + words[1] + " organization like the " + " \n" +
+		      "US government uses spies to infiltrate " + words[2] + " groups for the " + "\n" +
+		      "purpose of obtaining top secret " + words[3] + ". For example, " + "\n" +
+		      "spies might have to crack the code for accessing confidential, " + "\n" +
+		      words[4] + " files, or their mission could be far more dangerous -- " + "\n" +
+		      "like stealing the key ingredient for making " + words[5] + "'s award-winning" + "\n" +
+		      "Explosive Fudgy " + words[6] + ". Spies are found all over (the) " + words[7] + "\n" +
+		      "-- but they are not allowed to reveal their " + words[8] + " identities." + "\n" +
+		      "A teacher, " + words[9] + ", or even the little old " + words[10] + " with the cane" + "\n" +
+		      "and fifteen pet " + words[11] + " who lives next door to you could be a spy." + "\n" +
+		      "The world of spying might seem glamorous and " + words[12] + " but it's filled" + "\n" +
+		      "with risks and " + words[13] + "! Sure, spies have a never-ending supply of" + "\n" +
+		      "super cool electronic " + words[14] + ", but they can't trust any " + words[15] + "\n" +
+		      "which is why the number one rule of spies is to keep friends close and " + words[16] + " closer!")
+		blankLine()
+		print("The end.")
+
+	def madLib_3():
+		words = []
+
+		words.append(input("Adjective: "))
+		words.append(input("Adjective: "))
+		words.append(input("Noun: "))
+		words.append(input("Plural noun: "))
+		words.append(input("Noun: "))
+		words.append(input("Plural noun: "))
+		words.append(input("Adjective: "))
+		words.append(input("Plural noun: "))
+		words.append(input("Plural noun: "))
+		words.append(input("Verb: "))
+		words.append(input("Noun: "))
+		words.append(input("Verb: "))
+		words.append(input("Adjective: "))
+		words.append(input("Plural noun: "))
+		words.append(input("Noun: "))
+		words.append(input("Place: "))
+
+		words = [x.upper() for x in words]
+
+		blankLine()
+
+		print("All Done!")
+
+		blankLine()
+
+
+		print("Who is the Real Easter Bunny?:")
+		blankLine()
+		print("How much do you really know about the " + words[0] + " Easter Bunny?" + "\n" +
+			  "Here's the " + words[1] + " scoop of this mysterious " + words[2] + "." + "\n" +
+			  "Q: Does the Easter Bunny have magical " + words[3] + "?" + "\n" +
+			  "A: Absolutely. How else could one furry " + words[4] + " deliver candy" + "\n" +
+			  "filled " + words[5] + " to children everywhere in one " + words[6] + " night?" + "\n" +
+			  "Q: Santa has elves and reindeer to help him make and deliver " + words[7] + "\n" +
+			  "on Christmas. Does the Easter Bunny have any right-hand " + words[8] + "?" + "\n" +
+			  "A: As far as we know, the Easter Bunny likes to " + words[9] + " alone." + "\n" +
+			  "Like other bunnies, he is a very shy " + words[10] + " and prefers to" + "\n" +
+			  words[11] + " all by himself." + "\n" + 
+			  "Q: Where does the Easter Bunny live?" + "\n" +
+			  "A: Only the Easter Bunny knows the answer to this " + words[12] + " question, though"+ "\n" +
+			  "some " + words[13] + " believe he lives in a cozy " + words[14] + " in (the) " + words[15] + ".")
+		blankLine()
+		print("The end.")
+
+	if(story.upper() == "ALIEN ABDUCTION"):
+		madLib_1()
+	elif(story.upper() == "THE ART OF ESPIONAGE"):
+		madLib_2()
+	elif(story.upper() == "WHO IS THE REAL EASTER BUNNY?"):
+		madLib_3()
+	else:
+		print("Hmm... we don't see to have that story. Please try again.")
+		
+print("Welcome to MadLib Simulator!")
+blankLine()
+ans = input("Do you want to play a game? Y/N: ")
+
+if(ans.upper() == "Y"):
+	print("Great! Before we begin, let's go over some things.")
+	blankLine()
+	before = """
+			 What is a Madlib and how do I play?
+
+			 	A MadLib is a fun game to play with your friends! 
+			 	Player one prompts player two to fill in blanks of
+			 	a story, with out giving any context about the story.
+			 	After all the blanks are filled with the proper word class,
+			 	Player one will read the story outloud.  
+
+			 	In this situation, I will be player one and you will be player two.
+			 """
+	print(before)
+	wordClass = """
+			What are word classes?
+
+			I'm glad you asked!
+
+				Noun: A person, place or thing.
+					Example: man, horse, house
+				Verb: An action 
+					Example: run, walk, jump
+				Adverb: Describes a verb
+					Example: fast, slow, quickly
+				Adjective: Descriptive word
+					Example: ugly, pretty, green
+
+			I think we're ready to start! Just remember to press enter after each
+			input! :-)
+				"""
+	print(wordClass)
+	main()
+else:
+	quit()
+
+again = input("Do you want to play again? Y/N: ")
+
+if(again.upper() == "Y"):
+	main()
+else:
+	print("Thank you for playing! Please come again!")
